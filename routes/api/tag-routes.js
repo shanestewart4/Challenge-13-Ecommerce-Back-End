18c2be2e5e9166e89a990766061eb9a6fe5aa522
shanestewart4 <shanestewartguitar@gmail.com>
2021-01-28 16:39:22 -0600

    update to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  Tags.create(req.body)
+  Tag.create(req.body)
     .then((tag) => res.status(200).json(tag))
     .catch((err) =>res.status(404).json(err));
 });
